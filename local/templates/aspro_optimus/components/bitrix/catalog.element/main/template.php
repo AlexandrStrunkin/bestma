92ef54320840790165e116c6136ce1a0dbe8584e
Alexandr <as@webgk.ru>
2017-01-18 14:44:40 +0300

    SUP-5729: fix detail page url and catalog element

--- local/templates/aspro_optimus/components/bitrix/catalog.element/main/template.php
+++ local/templates/aspro_optimus/components/bitrix/catalog.element/main/template.php
@@ -387,9 +387,16 @@ setViewedProduct(<?=$arResult['ID']?>, <?=CUtil::PhpToJSObject($arViewedData, fa
 								</div>
 							<?}?>
 						<?}else{?>
+                            <?if(strlen($arResult["PRICES"][$arParams["RECOMENDED_RETAIL_PRICE"]]["PRINT_DISCOUNT_VALUE"])) {?>
+                                <div class="price_name"><?=GetMessage("RECOMENDED_RETAIL_PRICE");?></div>
+                                <div class="price rrp">                                
+                                        <?=$arResult["PRICES"][$arParams["RECOMENDED_RETAIL_PRICE"]]["PRINT_DISCOUNT_VALUE"];?>                           
+                                </div>                            
+                            <?}?> 
 							<?
 							$arCountPricesCanAccess = 0;
 							$min_price_id=0;
+                            
 							foreach( $arResult["PRICES"] as $key => $arPrice ) { if($arPrice["CAN_ACCESS"]){$arCountPricesCanAccess++;} }?>
 							<?foreach($arResult["PRICES"] as $key => $arPrice){?>
 								<?if($arPrice["CAN_ACCESS"] && ($key != $arParams["RECOMENDED_RETAIL_PRICE"])){
@@ -435,13 +442,7 @@ setViewedProduct(<?=$arResult['ID']?>, <?=CUtil::PhpToJSObject($arViewedData, fa
 									<?}?>
 								<?}?>
 							<?}?>
-						<?}?>
-                        <?if(strlen($arResult["PRICES"][$arParams["RECOMENDED_RETAIL_PRICE"]]["PRINT_DISCOUNT_VALUE"])) {?>
-                            <div class="price_name"><?=GetMessage("RECOMENDED_RETAIL_PRICE");?></div>
-                            <div class="price">                                
-                                    <?=$arResult["PRICES"][$arParams["RECOMENDED_RETAIL_PRICE"]]["PRINT_DISCOUNT_VALUE"];?>                           
-                            </div>                            
-                        <?}?>                                                                                         
+						<?}?>                                                                                        
 					</div>
 					<?if($arParams["SHOW_DISCOUNT_TIME"]=="Y"){?>
 						<?$arDiscounts = CCatalogDiscount::GetDiscountByProduct( $arResult["ID"], $USER->GetUserGroupArray(), "N", $min_price_id, SITE_ID );
